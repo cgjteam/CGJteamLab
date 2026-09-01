c6d7766083c15919ba9604dc92c6e1c44db48986
cgjteam <cgjteam@proton.me>
2026-09-01 21:44:51 +0200

    Delete CGJteamLab/Docs/Book1/build-docs.bat

--- CGJteamLab/Docs/Book1/build-docs.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-for %%f in (Proposition??.tex) do pdflatex -interaction=nonstopmode -halt-on-error "%%f"
-for %%f in (Proposition??.tex) do pdflatex -interaction=nonstopmode -halt-on-error "%%f"
-
-pdflatex -interaction=nonstopmode -halt-on-error Book1R.tex
-pdflatex -interaction=nonstopmode -halt-on-error Book1R.tex
